95bd2bbc39e46faf5bba551cdc40319f08bc0996
Shaksham Tatawat <shaksham.tatawat@shl.com>
2024-09-23 00:01:27 +0530

    Your commit message

--- script.js
+++ script.js
@@ -38,3 +38,10 @@ totalSlides++;
 
 
 
+// JavaScript to toggle the hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
